7cb3369c34a971bfab595625b2aedc60b364de0d
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-03 21:27:18 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   computers_random_number = rand(1..6)
-  guess = gets.chomp
+  guess = gets.chomp.to_i
   if guess == 'exit'
     puts "Goodbye!"
   elsif guess == computers_random_number
